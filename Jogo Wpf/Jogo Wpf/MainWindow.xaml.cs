bec7ccbc8f41c2a4121dd4f822f76a1a21a8c669
LucasSilva321 <luckkass89@gmail.com>
2017-10-10 17:18:44 -0300

    Layout Concluido

---
+++ Jogo Wpf/Jogo Wpf/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jogo_Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            imgBackground1.Width = this.Width;
+            imgBackground2.Width = this.Width;
+            imgBackground1.Margin = new Thickness(0, 0, 0, 0);
+            imgBackground2.Margin = new Thickness(-imgBackground2.Width, 0, 0, 0);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Right)
+            {
+
+
+                imgObstaculoUp.Margin = new Thickness(imgObstaculoUp.Margin.Left - 10, imgObstaculoUp.Margin.Top, 0, 0);
+                imgBackground1.Margin = new Thickness(imgBackground1.Margin.Left - 10, 0, 0, 0);
+                imgBackground2.Margin = new Thickness(imgBackground2.Margin.Left - 10, 0, 0, 0);
+                imgPersonagem.Margin = new Thickness(imgPersonagem.Margin.Left+5, imgPersonagem.Margin.Top, 0, 0);
+                if (imgBackground1.Margin.Left <= -imgBackground1.Width)
+                    imgBackground1.Margin = new Thickness(imgBackground1.Width - 50, 0, 0, 0);
+                if (imgBackground2.Margin.Left <= -imgBackground2.Width)
+                    imgBackground2.Margin = new Thickness(imgBackground2.Width - 50, 0, 0, 0);
+                if (imgPersonagem.Margin.Left >= this.Width -50)
+                {
+                    imgPersonagem.Margin = new Thickness(0, imgPersonagem.Margin.Top, 0, 0);
+                }
+                BitmapImage img = new BitmapImage(new Uri("Imagens/andando" + (i) + ".png", UriKind.RelativeOrAbsolute));
+                imgPersonagem.Source = img;
+                i = (i + 1) % 9;
+                ScaleTransform x = new ScaleTransform(1, 1);
+                imgPersonagem.RenderTransform = x;
+
+            }
+            if (e.Key == Key.Left)
+            {
+                imgObstaculoUp.Margin = new Thickness(imgObstaculoUp.Margin.Left + 10, imgObstaculoUp.Margin.Top, 0, 0);
+                imgPersonagem.Margin = new Thickness(imgPersonagem.Margin.Left - 5, imgPersonagem.Margin.Top, 0, 0);
+                imgBackground1.Margin = new Thickness(imgBackground1.Margin.Left + 10, 0, 0, 0);
+                imgBackground2.Margin = new Thickness(imgBackground2.Margin.Left + 10, 0, 0, 0);
+                
+                if (imgPersonagem.Margin.Left <= -imgPersonagem.Width + 80)
+                {
+                    imgPersonagem.Margin = new Thickness(this.Width, imgPersonagem.Margin.Top, 0, 0);
+                }
+                if (imgBackground1.Margin.Left >= imgBackground1.Width)
+                    imgBackground1.Margin = new Thickness(-imgBackground1.Width + 50, 0, 0, 0);
+                if (imgBackground2.Margin.Left >= imgBackground2.Width)
+                    imgBackground2.Margin = new Thickness(-imgBackground2.Width + 50, 0, 0, 0);
+                
+                BitmapImage img = new BitmapImage(new Uri("Imagens/andando" + (i) + ".png", UriKind.RelativeOrAbsolute));
+                imgPersonagem.Source = img;
+                i = (i + 1) % 9;
+                ScaleTransform x = new ScaleTransform(-1, 1);
+                imgPersonagem.RenderTransform = x;
+
+            }
+        }
+    }
+}
